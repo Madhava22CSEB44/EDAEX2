c73b87134f2b76cec3805aeed49b3b38def27649
Madhava22CSEB44 <121349785+Madhava22CSEB44@users.noreply.github.com>
2024-08-01 22:23:45 +0530

    Create Ex2.R

---
+++ Ex2.R
@@ -0,0 +1,17 @@
+data <- read.csv("CO2.csv")
+result <- t.test(data$conc,mu=550,alternative = "less")
+print(result)
+data$Treatment <- as.factor(data$Treatment)
+data$Type <- as.factor(data$Type)
+result2 <- t.test(uptake~Treatment,data=data)
+print(result2)
+result3 <- t.test(uptake~Treatment,data=data,var.equal = TRUE)
+print(result3)
+chill <- data$uptake[data$Treatment=="chilled"]
+print(chill)
+nchill <- data$uptake[data$Treatment=="nonchilled"]
+print(nchill)
+result4=t.test(chill,nchill,paired=TRUE)
+print(result4)
+result5 <- t.test(uptake~Type,data=data)
+print(result5)
